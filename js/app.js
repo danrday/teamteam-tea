b33257084c52e4724dc2b7a2bc64b975495eade6
Tim Creasy <timcreasy@me.com>
2016-05-24 15:47:48 -0500

    Added javascript

--- js/app.js
+++ js/app.js
@@ -1,76 +1,77 @@
 var productArray = [
 
   {
-    name: "ProductA",
-    description: "blah blah blah",
-    price: 4.99,
+    name: "John Lemmon",
+    description: "This zesty blend will have you yearning for more.  John Lennon famously wrote his hit single 'Imagine' while under the influence of it's warm haze.",
+    price: "$5.99/oz",
     productURL: "...."
   },
   {
-    name: "ProductA",
-    description: "blah blah blah",
-    price: 4.99,
+    name: "Blissfully Green",
+    description: "With roots in the indigenous communes of Australia, this hearty blend has hints of lavender and rosemary.",
+    price: "$3.99/oz",
     productURL: "...."
   },
   {
-    name: "ProductA",
-    description: "blah blah blah",
-    price: 4.99,
+    name: "Oragami Orange",
+    description: "Dried citrus with hints of elderberry, Oragami Orange is a favorite among our customers.",
+    price: "$5.99/oz",
     productURL: "...."
   },
   {
-    name: "ProductA",
-    description: "blah blah blah",
-    price: 4.99,
+    name: "Soulful Black",
+    description: "Perfectly flavored with natural bergamont, take it wherever you may go.",
+    price: "6.99/oz",
     productURL: "...."
   },
   {
-    name: "ProductA",
-    description: "blah blah blah",
-    price: 4.99,
+    name: "Rasberry Delight",
+    description: "A gentle and organic blend, meant to target and strengthen the immune system.",
+    price: "6.99/oz",
     productURL: "...."
   },
   {
-    name: "ProductA",
-    description: "blah blah blah",
-    price: 4.99,
+    name: "Japenese Goji Berry",
+    description: "Rich in antioxidants, goji berry brings a slight tartness and bright citrus flavor.",
+    price: "8.99/oz",
     productURL: "...."
   },
   {
-    name: "ProductA",
-    description: "blah blah blah",
-    price: 4.99,
+    name: "Aged Earl Grey",
+    description: "A light and aromatic blend of fine black tea.",
+    price: "$2.99/oz",
     productURL: "...."
   },
   {
-    name: "ProductA",
-    description: "blah blah blah",
-    price: 4.99,
+    name: "Moroccan Mint",
+    description: "An invigorating infusion of peppermint and spearming, designed to wake you up.",
+    price: "$7.99/oz",
     productURL: "...."
   },
   {
-    name: "ProductA",
-    description: "blah blah blah",
-    price: 4.99,
+    name: "White Rose",
+    description: "Our White Rose tea invokes the sweet lingering fragrance of late summer roses winding up a garden trellis.",
+    price: "6.99/oz",
     productURL: "...."
   },
   {
-    name: "ProductA",
-    description: "blah blah blah",
-    price: 4.99,
+    name: "Golden Jasmine",
+    description: "Naturally scented with a distinct fresh jasmine flower aroma and once infused produces an invitingly fresh golden tea.",
+    price: "4.99/oz",
     productURL: "...."
   },
   {
-    name: "ProductA",
-    description: "blah blah blah",
-    price: 4.99,
+    name: "Blueberry Green",
+    description: "A delightful blend of natural blueberry flavors and permium Ceylon tea.",
+    price: "12.99/oz",
     productURL: "...."
   },
   {
-    name: "ProductA",
-    description: "blah blah blah",
-    price: 4.99,
+    name: "Dragonwell",
+    description: "This is an authentic Chinese West Lake Dragonwell and considered to be one of the world's finest green teas.",
+    price: "7.99/oz",
     productURL: "...."
   }
 
 ];
+
