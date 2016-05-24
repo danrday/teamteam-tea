37b89961040eb0f4f698f01313a02b8a8aee9f57
Tim Creasy <timcreasy@me.com>
2016-05-24 15:13:09 -0500

    Nothing

---
+++ js/app.js
@@ -0,0 +1,76 @@
+var productArray = [
+
+  {
+    name: "ProductA",
+    description: "blah blah blah",
+    price: 4.99,
+    productURL: "...."
+  },
+  {
+    name: "ProductA",
+    description: "blah blah blah",
+    price: 4.99,
+    productURL: "...."
+  },
+  {
+    name: "ProductA",
+    description: "blah blah blah",
+    price: 4.99,
+    productURL: "...."
+  },
+  {
+    name: "ProductA",
+    description: "blah blah blah",
+    price: 4.99,
+    productURL: "...."
+  },
+  {
+    name: "ProductA",
+    description: "blah blah blah",
+    price: 4.99,
+    productURL: "...."
+  },
+  {
+    name: "ProductA",
+    description: "blah blah blah",
+    price: 4.99,
+    productURL: "...."
+  },
+  {
+    name: "ProductA",
+    description: "blah blah blah",
+    price: 4.99,
+    productURL: "...."
+  },
+  {
+    name: "ProductA",
+    description: "blah blah blah",
+    price: 4.99,
+    productURL: "...."
+  },
+  {
+    name: "ProductA",
+    description: "blah blah blah",
+    price: 4.99,
+    productURL: "...."
+  },
+  {
+    name: "ProductA",
+    description: "blah blah blah",
+    price: 4.99,
+    productURL: "...."
+  },
+  {
+    name: "ProductA",
+    description: "blah blah blah",
+    price: 4.99,
+    productURL: "...."
+  },
+  {
+    name: "ProductA",
+    description: "blah blah blah",
+    price: 4.99,
+    productURL: "...."
+  }
+
+];
